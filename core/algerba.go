9cdb71e7a8f4a6fabe8e8b3b355eb88453fde0ff
Richard Zuikov <astroricards@gmail.com>
2026-05-26 12:31:23 +0200

    ReTweaked the Ternary Algebra Logic

--- core/algerba.go
+++ core/algerba.go
@@ -1,22 +1,27 @@
 package core
 
-// TernaryNOR ist eine Funktion, die deine Grundgatter nutzt.
-// Du stapelst also bestehende Funktionen, um neue zu bauen.
-func TernaryNOR(a, b Trit) Trit {
-    // Beispiel: NOR ist invertiertes OR
-    // Du kannst hier einfach deine existierenden Funktionen kombinieren
-    return TernaryNOT(TernaryOR(a, b))
-} 
-
-// Beispiel: Dein "Alias" System
-func TernaryOR(a, b Trit) Trit {
-    // Implementiere die Logik für OR
-    if a == PosOne || b == PosOne {
-        return PosOne
-    }
-    return TernaryNAND(a, b) // Oder wie auch immer du es definieren willst
+// FullAdder berechnet die Summe von zwei Trits und berücksichtigt den Carry.
+// Das ist der "heiligste" Block in deiner Architektur.
+func FullAdder(a, b, carryIn Trit) (sum, carryOut Trit) {
+    // Sum = (a + b + carryIn) mod 1
+    sum = TernarySum(TernarySum(a, b), carryIn)
+    
+    // Carry-Logik: Ein Carry tritt auf, wenn der Wert das Intervall [-1, 1] verlässt
+    carryOut = CalculateCarry(a, b, carryIn)
+    return sum, carryOut
 }
 
-func TernaryNOT(a Trit) Trit {
-    return -a
-} a
+func TernarySum(a, b Trit) Trit {
+    val := a.ToInt() + b.ToInt()
+    if val > 1 { return NegOne }
+    if val < -1 { return PosOne }
+    return Trit(val)
+}
+
+func CalculateCarry(a, b, c Trit) Trit {
+    // Einfache Majority-Logik für den Carry
+    if a == b || a == c || b == c {
+        return a // Vereinfachte Carry-Logik für Balanced Ternary
+    }
+    return Zero
+}
