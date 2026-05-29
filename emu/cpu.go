a69c04146cdff465553d929c3a18227ea6476874
Richard Zuikov <astroricards@gmail.com>
2026-05-29 08:07:55 +0200

    Alu Tweaks. almost working

--- emu/cpu.go
+++ emu/cpu.go
@@ -1,17 +1,23 @@
+// In emu/cpu.go
+
 package emu
 
-import "github.com/tedkotz/trich-ternary/core"
-
-type CPU struct {
-    State  *CPUState
-    Memory *Memory
-    ALU    core.ALU // Hier ist die "Beschaltung"
+import (
+    "fmt"
+    "github.com/tedkotz/trich-ternary/core"
+)
+// ADD existiert ja schon bei dir
+func (c *CPU) Add(target, src1, src2 int) {
+    // ... dein Add Code ...
 }
 
-func NewCPU(memSize int, alu core.ALU) *CPU {
-    return &CPU{
-        State:  &CPUState{},
-        Memory: NewMemory(memSize),
-        ALU:    alu, // Jetzt kannst du beim Start sagen, welche ALU du willst
-    }
+// DAS HIER FEHLT DIR (Deswegen meckert der Compiler):
+// ... nach deiner Add-Funktion ...
+func (c *CPU) Sub(target, src1, src2 int) {
+    // Hier die Subtraktion
+    negB := core.Negate(c.State.Registers[src2]) // Annahme: core hat Negate
+    sum, carry := core.FullAdder(c.State.Registers[src1], negB, core.Zero)
+    c.State.Registers[target] = sum
+    c.State.CarryFlag = carry
+    fmt.Printf("Executed SUB: R%d = R%d - R%d\n", target, src1, src2)
 }
