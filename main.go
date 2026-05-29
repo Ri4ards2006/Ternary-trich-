1f545562d23780b2bfdb6cd05bd900dcd5ff243d
Richard Zuikov <astroricards@gmail.com>
2026-05-29 11:06:32 +0200

    Made it Interactive

--- main.go
+++ main.go
@@ -7,13 +7,26 @@ import (
 )
 
 func main() {
-    // Da Memory & RegisterBank jetzt im Paket emu sind, 
-    // und NewCPU sie korrekt initialisiert, brauchst du sie hier nicht direkt
     cpu := emu.NewCPU(256, 8, 4)
-
+    
+    // Beispiel-Werte setzen
     cpu.Registers.Data[1][0] = core.PosOne
-    cpu.Registers.Data[2][0] = core.PosOne
+    cpu.Registers.Data[2][0] = core.NegOne
 
-    cpu.Add(0, 1, 2)
-    fmt.Println("Berechnung abgeschlossen.")
+    fmt.Println("--- Ternary Hardware Configurator ---")
+    fmt.Println("Wähle Operation: ADD oder SUB")
+    
+    var op string
+    fmt.Scanln(&op)
+
+    switch op {
+    case "ADD":
+        cpu.Add(0, 1, 2)
+    case "SUB":
+        cpu.Sub(0, 1, 2)
+    default:
+        fmt.Println("Unbekannte Operation!")
+    }
+    
+    fmt.Printf("Ergebnis in R0: %s\n", cpu.Registers.Data[0][0])
 }
