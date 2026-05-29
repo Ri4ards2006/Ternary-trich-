1c302024c491f757535c99f8c10c869fc74f3683
Richard Zuikov <astroricards@gmail.com>
2026-05-29 09:03:04 +0200

    Kinda bugging better or sum

--- emu/cpu.go
+++ emu/cpu.go
@@ -5,6 +5,28 @@ import (
     "github.com/tedkotz/trich-ternary/core"
 )
 
+// --- Alles hier vereint, damit Go nicht mehr "undefined" schreit ---
+
+type Memory struct {
+    cells []core.Trit
+}
+
+func NewMemory(size int) *Memory {
+    return &Memory{cells: make([]core.Trit, size)}
+}
+
+type RegisterBank struct {
+    Data [][]core.Trit
+}
+
+func NewRegisterBank(numRegs, wordSize int) *RegisterBank {
+    data := make([][]core.Trit, numRegs)
+    for i := range data {
+        data[i] = make([]core.Trit, wordSize)
+    }
+    return &RegisterBank{Data: data}
+}
+
 type CPUState struct {
     Registers [][]core.Trit
     PC        int
@@ -26,11 +48,15 @@ func NewCPU(memSize, numRegs, wordSize int) *CPU {
     }
 }
 
+func (c *CPU) Add(target, src1, src2 int) {
+    sum, _ := core.FullAdder(c.Registers.Data[src1][0], c.Registers.Data[src2][0], core.Zero)
+    c.Registers.Data[target][0] = sum
+    fmt.Printf("Executed ADD: R%d = R%d + R%d\n", target, src1, src2)
+}
+
 func (c *CPU) Sub(target, src1, src2 int) {
-    // A - B = A + (-B). Da wir hier nur ein Trit haben: -val
     val2 := c.Registers.Data[src2][0]
-    negVal2 := core.Trit(-val2.ToInt()) 
-    
+    negVal2 := core.Trit(-val2.ToInt())
     sum, _ := core.FullAdder(c.Registers.Data[src1][0], negVal2, core.Zero)
     c.Registers.Data[target][0] = sum
     fmt.Printf("Executed SUB: R%d = R%d - R%d\n", target, src1, src2)
--- main.go
+++ main.go
@@ -1,37 +1,22 @@
-package emu
+package main
 
 import (
     "fmt"
     "github.com/tedkotz/trich-ternary/core"
+    "github.com/tedkotz/trich-ternary/emu"
 )
 
-// CPUState muss hier sein, damit cpu.go es kennt
-type CPUState struct {
-    Registers [][]core.Trit
-    PC        int
-    CarryFlag core.Trit
-    Halted    bool
-}
-
-type CPU struct {
-    State     *CPUState
-    Memory    *Memory
-    Registers *RegisterBank
-}
-
-// ACHTUNG: Die Methoden müssen GROSS geschrieben sein (Add, Sub), 
-// sonst sieht isa.go sie nicht!
-
-func (c *CPU) Add(target, src1, src2 int) {
-    sum, _ := core.FullAdder(c.Registers.Data[src1][0], c.Registers.Data[src2][0], core.Zero)
-    c.Registers.Data[target][0] = sum
-    fmt.Printf("Executed ADD: R%d = R%d + R%d\n", target, src1, src2)
-}
-
-func (c *CPU) Sub(target, src1, src2 int) {
-    val2 := c.Registers.Data[src2][0]
-    negVal2 := core.Trit(-val2.ToInt()) 
-    sum, _ := core.FullAdder(c.Registers.Data[src1][0], negVal2, core.Zero)
-    c.Registers.Data[target][0] = sum
-    fmt.Printf("Executed SUB: R%d = R%d - R%d\n", target, src1, src2)
+func main() {
+    fmt.Println("--- Ternary Hardware Configurator ---")
+    
+    // WICHTIG: Präfix "emu." vor den Typen!
+    cpu := emu.NewCPU(256, 8, 4) 
+    
+    // Hier musst du den Zugriff auf Register prüfen, 
+    // falls "Registers" ein Struct ist, der "Data" enthält
+    cpu.Registers.Data[1][0] = core.PosOne
+    cpu.Registers.Data[2][0] = core.PosOne
+    
+    fmt.Println("CPU initialisiert.")
+    cpu.Add(0, 1, 2)
 }
